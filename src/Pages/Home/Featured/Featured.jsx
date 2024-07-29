9f5c28676e318d4683a5a9fb480d584c59e70519
hossan23 <imamhossan101@gmail.com>
2024-07-29 00:33:57 -0400

    added featured section in home page

---
+++ src/Pages/Home/Featured/Featured.jsx
@@ -0,0 +1,33 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg";
+import "./Featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured-style text-white">
+      <SectionTitle
+        subHeading="Check it out"
+        heading="featured item"
+      ></SectionTitle>
+      <div className="flex justify-between items-center space-x-10  p-10 ">
+        <div>
+          <img src={featuredImg} alt="" />
+        </div>
+        <div>
+          <h3>March 20, 2023</h3>
+          <p>
+            WHERE CAN I GET SOME? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Error voluptate facere, deserunt dolores maiores
+            quod nobis quas quasi. Eaque repellat recusandae ad laudantium
+            tempore consequatur consequuntur omnis ullam maxime tenetur.
+          </p>
+          <button className="text-white border-b-2 p-4 rounded-lg text-lg font-bold hover:bg-black">
+            Read More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Menu from "../Menu/Menu";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <Category />
       <Menu />
+      <Featured />
     </>
   );
 };
